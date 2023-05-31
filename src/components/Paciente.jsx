442bdcc1c012b6819f4f92745f3db59363d98863
Carlos Hernández <carlos_develops@outlook.com>
2023-05-31 11:05:56 -0600

    estilizando componente Listado de Pacientes

---
+++ src/components/Paciente.jsx
@@ -0,0 +1,28 @@
+const Paciente = () => {
+  return (
+    <div className="m-3 bg-white shadow-md px-5 py-10 rounded">
+        <p className="font-bold mb-3 text-gray-700 uppercase">
+            Propietario: {""}
+            <span className="font-normal normal-case">Carlos</span>
+        </p>
+        <p className="font-bold mb-3 text-gray-700 uppercase">
+            Email: {""}
+            <span className="font-normal normal-case">
+                carlos_develops@outlook.com
+            </span>
+        </p>
+        <p className="font-bold mb-3 text-gray-700 uppercase">
+            Alta: {""}
+            <span className="font-normal normal-case">10 Diciembre 2023</span>
+        </p>
+        <p className="font-bold mb-3 text-gray-700 uppercase">
+            Sintomas: {""}
+            <span className="font-normal normal-case">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+            </span>
+        </p>
+    </div>
+  )
+}
+
+export default Paciente
