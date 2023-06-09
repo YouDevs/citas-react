0a8f4c2a01adf9c902dc41326e25579456219a41
Carlos Hernández <carlos_develops@outlook.com>
2023-06-09 10:03:13 -0600

    guardar pacientes en localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
 /* eslint-disable no-unused-vars */
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Header from "./components/Header";
 import Formulario from "./components/Formulario";
 import ListadoPacientes from "./components/ListadoPacientes";
 
 function App() {
-	const [pacientes, setPacientes] = useState([]);
+	const [pacientes, setPacientes] = useState(() => {
+		return JSON.parse(localStorage.getItem('pacientes')) ?? []
+	});
 	const [paciente, setPaciente] = useState({});
+
+	// useEffect(() => {
+	// 	console.log('algo en local storage?')
+	// 	console.log(localStorage.getItem('pacientes'))
+	// 	const obtenerLS = () => {
+	// 		const pacientesLS = JSON.parse(localStorage.getItem('pacientes')) ?? []
+	// 		setPacientes(pacientesLS)
+	// 	}
+
+	// 	obtenerLS()
+	// }, [])
+
+	useEffect(() => {
+		localStorage.setItem('pacientes', JSON.stringify(pacientes))
+	}, [pacientes])
 
 	const eliminarPaciente = (id) => {
 		// filtramos para dejar en el estado a los pacientes que no tienen el id que queremos eliminar.
